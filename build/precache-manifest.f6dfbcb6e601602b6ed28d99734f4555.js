2e26e199c4b628e7c599c7ffca8520f779800117
Orlando Gelves <orlandoantoni@hotmail.com>
2020-09-22 20:05:52 -0500

    Lab 5 finalizado

---
+++ build/precache-manifest.f6dfbcb6e601602b6ed28d99734f4555.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed4575c3fecf08ddaa1868d94f31cc9c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a18ce5aa826852f66eeb",
+    "url": "/static/css/2.d2d88084.chunk.css"
+  },
+  {
+    "revision": "80bb458119b6107d5608",
+    "url": "/static/css/main.f7fad5a9.chunk.css"
+  },
+  {
+    "revision": "a18ce5aa826852f66eeb",
+    "url": "/static/js/2.511c1701.chunk.js"
+  },
+  {
+    "revision": "62f5de1a561ffce7e39aef745f889ab0",
+    "url": "/static/js/2.511c1701.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80bb458119b6107d5608",
+    "url": "/static/js/main.d54c1f3e.chunk.js"
+  },
+  {
+    "revision": "47c829c4216f4d43c974",
+    "url": "/static/js/runtime-main.0f14c774.js"
+  }
+]);
